5d58bd66777e9208f01c7b73be04411925d6bfcc
unknown <dinglasan.harold.ian.dave@gmail.com>
2025-03-05 16:04:00 +0800

    Modified Register and Request Form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import RequestForm from "./pages/RequestForm";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
       <Router>
           <Routes>
               <Route path="/request/form" element={< RequestForm/>} />
-              
+              <Route path="/register/form" element={<Register/>} />
               <Route path="*" element={<Navigate to="/request/form" />} />
           </Routes>
       </Router>
